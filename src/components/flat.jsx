08d79402c1425a0187ccf2573bc85cf86d6dacb2
xinglin2016 <lixinglin0829@gmail.com>
2021-05-07 11:13:42 -0400

    improve style for flat

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -7,9 +7,11 @@ class Flat extends Component {
   }
 
   render () {
-
+    const style = {
+      backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url('${this.props.flat.imageUrl}')`
+    }
     return (
-      <div className={`card${this.props.selected ? ' active' : ''}`} style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url('${this.props.flat.imageUrl}')` }}>
+      <div className={`card${this.props.selected ? ' active' : ''}`} style={style}>
         <div className="card-category">{this.props.flat.price} {this.props.flat.priceCurrency}</div>
         <div className="card-description">
           <h2>{this.props.flat.name}</h2>
